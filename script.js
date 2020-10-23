1109d46f81b20df4d227d5e41d703a2a5e4ef570
jonnahmarie <jonnah.marie@gmail.com>
2020-10-23 00:23:45 -0700

    adding variables and conditionals

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ function writePassword() {
 
   passwordText.value = password;
 
+  // Prompt to enter character length
+
+  var passwordLength = prompt("Choose a number between 8 and 128");
+
+  // Conditions to generate password
+
+  function generatePassword () {
+    if ((passwordLength >= "8" && passwordLength <= "128")) {
+      var lowercaseLetters = confirm("Would you like to add lowercase letters?");
+      var uppercaseLetters = confirm("Would you like to add  uppercase letters?");
+      var numericCharacters = confirm("Would you like to add numeric characters?");
+      var specialCharacters = confirm("Would you like to add special characters?");
+    } else ((passwordLength < 8 && passwordLength > 128)) {
+      alert("Please enter a number between 8 and 128");
+    }
+  }
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
