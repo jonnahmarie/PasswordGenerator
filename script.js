9f6408ab366c8fb9ca2946ce1720370a7bb386be
jonnahmarie <jonnah.marie@gmail.com>
2020-10-23 20:05:20 -0700

    redefining user input variables and that prompts display when button is clicked

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+
+  // Define user input variables
+  var passwordLength = parseInt(prompt("How many characters would you like your password to be? (Min. 8, Max. 128)"));
+
+  // User Validation
+  if ((passwordLength < 8 || passwordLength > 128)) {
+    alert("Please enter a valid number.");
+  };
+
+  var lowerInput = confirm("Would you like to include lowercase letters?");
+  var upperInput = confirm("Would you like to include UPPERcase letters?");
+  var numInput = confirm("Would you like to include numeric characters?");
+  var specialInput = confirm("Would you like to include special characters?");
+
+  
+}
+);
+
+
+
