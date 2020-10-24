98732cdb39004517d446cc0df9bec8a4f6b24e90
jonnahmarie <jonnah.marie@gmail.com>
2020-10-23 21:31:09 -0700

    added conditionals and character types

--- script.js
+++ script.js
@@ -11,19 +11,45 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function() {
+  
 
-  // Define user input variables
+    // Define user input variables
   var passwordLength = parseInt(prompt("How many characters would you like your password to be? (Min. 8, Max. 128)"));
+  var lowerInput;
+  var upperInput;
+  var numInput;
+  var symbolInput;
 
   // User Validation
-  if ((passwordLength < 8 || passwordLength > 128)) {
-    alert("Please enter a valid number.");
-  };
+  while ((passwordLength < 8 || passwordLength > 128)) {
+    prompt("Please enter a valid character number. How many characters would you like your password to be? (Min. 8, Max. 128");
 
-  var lowerInput = confirm("Would you like to include lowercase letters?");
-  var upperInput = confirm("Would you like to include UPPERcase letters?");
-  var numInput = confirm("Would you like to include numeric characters?");
-  var specialInput = confirm("Would you like to include special characters?");
+    if (passwordLength >= 8 || passwordLength <= 128) {
+      // Continues with user input of password criteria
+
+      lowerInput = confirm("Would you like to include lowercase letters?");
+      upperInput = confirm("Would you like to include uppercase letters?");
+      numInput = confirm("Would you like to include numbers?");
+      symbolInput = confirm("Would you like to include any special characters?");
+    }
+ } 
+
+  // character types
+
+  var lowercaseLetters = ["abcdefghijklmnopqrstuvwxyz"].split("");
+  var uppercaseLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].split("");
+  var numCharacters = parseInt(["1234567890"].split(""));
+  var symbolCharacters = ["!@#$%^&*()_+-={}[]\|:<>?;',./"].split("");
+
+  
+  
+  if (!lowerInput && !upperInput && !numInput && !specialInput) {
+    alert("Please choose criteria in order to generate this password!");
+  } else if (lowercaseLetters && uppercaseLetters && numCharacters && symbolCharacters) {
+
+  }
+
+  
 
   
 }
